6bc7ed580ad28a884f7a078b92c90bdc494410a1
Fabián Veliz <velizfabianhoracio@gmail.com>
2018-04-23 22:47:21 -0300

    Update schema

--- schema.js
+++ schema.js
@@ -1,12 +1,43 @@
 const { makeExecutableSchema } = require('graphql-tools')
 
 const typeDefs = `
+  # This is a course. It belongs to a teacher
   type Course {
     id: ID!
     title: String!
+    description: String!
+    teacher: Teacher
+    rating: Float
+    # Course comments
+    comments: [Comment]
   }
+
+  # This is a teacher. It has many courses
+  type Teacher {
+    id: ID!
+    name: String!
+    country: String!
+    gender: Gender
+    courses: [Course]
+  }
+
+  enum Gender {
+    MALE
+    FEMALE
+  }
+
+  type Comment {
+    id: ID!
+    # Author name
+    name: String!
+    body: String!
+  }
+
   type Query {
     Courses: [Course]
+    Teachers: [Teacher]
+    course(id: Int): Course
+    teacher(id: Int): Teacher
   }
 `
 
